132e79a8fa46103fda5385df0c5b5d4e2062e044
elbertwang3 <elbert_wang@alumni.brown.edu>
2017-09-30 04:16:27 -0700

    tooltip finally done lol

--- js/countries.js
+++ js/countries.js
@@ -1,20 +1,70 @@
 var margin = {top: 50, left: 50, right: 50, bottom: 50},
-	countriesheight = 650 - margin.top - margin.bottom,
-	countrieswidth = 1000 - margin.left - margin.right;
+	countriesheight = 650;
+	countrieswidth = 1000;
+
 
 var countriessvg = d3.select("#countriessvg")
+
+
+/*countriessvg.append("rect")
+    .attr("class", "background")
+    .attr("width", countrieswidth)
+    .attr("height", countriesheight)
+    .on("click", reset)*/
+
+var countriesg = countriessvg
 					.append("g")
-					.attr("transform", "translate(" + margin.left + ", " + margin.top + ")");
+					//.attr("transform", "translate(0, " + 130 + ")");
+
+countriessvg.append("g")
+  .attr("class", "legendOrdinal")
+  .attr("transform", "translate(" + countrieswidth/4 + ", " + (6*countriesheight/7) + ")");
+
+
+/*var countriestip = countriessvg.append("g")  
+        .attr("class", "countriestooltip")
+        .attr("width", 500)
+        .attr("height", 500)
+        .attr("fill", "black")
+       // .attr("transform", "translate(" + (countrieswidth/6) + ", " + (3*countriesheight/4) +")");*/
+countriestip = d3.select("#countriesgraph").append("div")  
+        .attr("class", "tooltip")
+        .style("left", 300 + "px")    
+        .style("top", 725 + "px");	
 
 var projection = d3.geoMercator()
 					.translate([countrieswidth/2, countriesheight/2]);
+
+var zoom = d3.zoom()
+    // no longer in d3 v4 - zoom initialises with zoomIdentity, so it's already at origin
+    // .translate([0, 0]) 
+    // .scale(1) 
+    .scaleExtent([1, 8])
+    .on("zoom", zoomed);
 
 var path = d3.geoPath()
 			.projection(projection);
 
 var countriesColor = d3.scaleOrdinal()
-			.domain(["PM", "PNM", "NPM", "NPNM"])
+			.domain(["Participating and medaled", "Participating but no medal", "Not participating but medaled", "Not participating and no medal"])
 			.range(["#00d0a1", "#64bdff", "#cccccc", "#f0f0f0"])
+
+var legendOrdinal = d3.legendColor()
+  //d3 symbol creates a path-string, for example
+  //"M0,-8.059274488676564L9.306048591020996,
+  //8.059274488676564 -9.306048591020996,8.059274488676564Z"
+  .shape('circle')
+  .shapePadding(150)
+  .orient('horizontal')
+  //use cellFilter to hide the "e" cell
+  .cellFilter(function(d){ return d.label !== "e" })
+  .scale(countriesColor);
+
+countriessvg.select(".legendOrdinal")
+  .call(legendOrdinal);
+
+countriessvg.call(zoom);
+
 d3.queue()
 	.defer(d3.json, "data/world-countries-sans-antarctica.json")
 	.defer(d3.csv, "data/gymnasts.csv")
@@ -28,17 +78,17 @@ function ready(error, countriesdata, gymnastsdata, medalsdata) {
 
 	var countriesParticipatingSet = new Set();
 	var countriesMedaledSet = new Set();
-	var countriesGymnasts = [];
+	var countriesGymnasts = {};
 	for (var i = 0; i < gymnastsdata.length; i++) {
 		
 		countryname = gymnastsdata[i].country;
 		gymnastname = gymnastsdata[i].gymnast
 		countryname = fixCapitalization(countryname);
 
-		if (!countriesParticipatingSet.has(countryname)) {
-			countriesGymnasts.push({country: countryname, gymnasts: [gymnastname]})
+		if (countriesGymnasts.hasOwnProperty(countryname)) {
+			countriesGymnasts[countryname].push(gymnastname)
 		} else {
-			countriesGymnasts[countriesParticipatingSet.size - 1].gymnasts.push(gymnastname)
+			countriesGymnasts[countryname] = [gymnastname]
 		}
 		countriesParticipatingSet.add(countryname);
 
@@ -55,7 +105,7 @@ function ready(error, countriesdata, gymnastsdata, medalsdata) {
 	countries = topojson.feature(countriesdata, countriesdata.objects.countries1).features
 	//console.log(countries);
 
-	countriessvg.selectAll(".country")
+	countriesg.selectAll(".country")
 		.data(countries)
 		.enter()
 		.append("path")
@@ -66,28 +116,54 @@ function ready(error, countriesdata, gymnastsdata, medalsdata) {
 				if (countriesMedaledSet.has(d.properties.name)) {
 					console.log("PM: " + d.properties.name)
 					console.log(countriesColor("PM"));
-					return countriesColor("PM");
+					return countriesColor("Participating and medaled");
 				}
 				else {
 					console.log("PNM: " + d.properties.name)
 					console.log(countriesColor("PNM"));
-					return countriesColor("PNM");
+					return countriesColor("Participating but no medal");
 				}
 			}
 			else {
 				if (countriesMedaledSet.has(d.properties.name)) {
 					console.log("NPM: " + d.properties.name)
-					console.log(countriesColor("NPM"));
+					console.log(countriesColor("Not participating but medaled"));
 					return countriesColor("NPM");
 				}
 				else {
 					console.log("NPNM: " + d.properties.name)
 					console.log(countriesColor("NPNM"));
-					return countriesColor("NPNM");
+					return countriesColor("Not participating and no medal");
 				}
 			}
-		});
-}
+		})
+		.on("mouseover", function(d) {
+			if (typeof countriesGymnasts[d.properties.name] !== "undefined") {	
+				console.log("getting here");
+				htmlstring = "<h5>" +  d.properties.name + "</h5> <ul>"
+				for (var i = 0; i < countriesGymnasts[d.properties.name].length; i++) {
+					htmlstring += "<li>" + countriesGymnasts[d.properties.name][i] + "</li>"
+				}
+				htmlstring += "</ul>"
+				
+				countriestip.html(htmlstring);
+		
+
+		
+				countriestip.transition()   
+					.duration(200)     
+            		.style("opacity", "1")
+          	}
+        })
+        .on("mouseout", function(d) {   
+	        countriestip.transition()    
+	            .duration(200)    
+	            .style("opacity", "0"); 
+  		});
+  }
+  
+
+
 
 function fixCapitalization(country) {
 	var splitCountry = country.split(" ")
@@ -100,5 +176,19 @@ function fixCapitalization(country) {
 		}
 	}
 	toReturn = toReturn.substring(0, toReturn.length - 1);
-	return toReturn
+	return toReturn;
+}
+
+function reset() {
+
+  countriessvg.transition()
+      .duration(750)
+      // .call( zoom.transform, d3.zoomIdentity.translate(0, 0).scale(1) ); // not in d3 v4
+      .call(zoom.transform, d3.zoomIdentity); // updated for d3 v4
+}
+
+function zoomed() {
+  //countriesg.style("stroke-width", 1.5 / d3.event.transform.k + "px");
+  // g.attr("transform", "translate(" + d3.event.translate + ")scale(" + d3.event.scale + ")"); // not in d3 v4
+  countriesg.attr("transform", d3.event.transform); // updated for d3 v4
 }
