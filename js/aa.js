c23c4fe4dd02f86f73e9f976a60e09d181e9de58
elbertwang3 <elbert_wang@alumni.brown.edu>
2017-10-01 01:22:40 -0700

    ok for now add choose later

--- js/aa.js
+++ js/aa.js
@@ -15,13 +15,22 @@ d3.csv("data/aa.csv", cast, function(data) {
   			return {
   				className: group.key,
   				axes: [
-  					{axis: "Vault", value: round3(group.value['vt'])},
-  					{axis: "Uneven Bars", value: round3(group.value['ub'])},
-  					{axis: "Balance Beam", value: round3(group.value['bb'])},
-  					{axis: "Floor Exercise", value: round3(group.value['fx'])}
+  					{axis: "VT", value: +round3(group.value['vt'])},
+  					{axis: "UB", value: +round3(group.value['ub'])},
+  					{axis: "BB", value: +round3(group.value['bb'])},
+  					{axis: "FX", value: +round3(group.value['fx'])}
   				]
   			}
   		})
+
+  	maxScoreData[0].axes[0]['yOffset'] = 10;
+  	maxScoreData[0].axes[0]['xOffset'] = -20;
+  	maxScoreData[0].axes[2]['xOffset'] = -20;
+  	maxScoreData[0].axes[2]['yOffset'] = -10;
+  	maxScoreData[0].axes[3]['xOffset'] = -45;
+  	maxScoreData[0].axes[3]['yOffset'] = -5;
+  	maxScoreData[0].axes[1]['yOffset'] = -5;
+
 
 
   	console.log(maxScoreData);
@@ -39,14 +48,84 @@ d3.csv("data/aa.csv", cast, function(data) {
   			return {
   				className: group.key,
   				axes: [
-  					{axis: "Vault", value: round3(group.value['vt'])},
-  					{axis: "Uneven Bars", value: round3(group.value['ub'])},
-  					{axis: "Balance Beam", value: round3(group.value['bb'])},
-  					{axis: "Floor Exercise", value: round3(group.value['fx'])}
+  					{axis: "VT", value: +round3(group.value['vt'])},
+  					{axis: "UB", value: +round3(group.value['ub'])},
+  					{axis: "BB", value: +round3(group.value['bb'])},
+  					{axis: "FX", value: +round3(group.value['fx'])}
   				]
   			}
   		})
   	console.log(avgScoreData);
+
+
+  	var radarwidth = 550,
+  		radarheight = 550;
+
+  	var superscore = d3.max(maxScoreData, function(d) { return d3.sum(d.axes, function (v) { return v.value; })})
+  	var radarColorScale = d3.scaleLinear()
+  							.domain([0, superscore])
+  							.range(["C2CAD6", "#08306b"]);
+
+  	d3.selectAll("input")
+		.on("change", selectDataset);
+	
+	
+  
+  	/*var mycfg = {
+  		w: radarwidth,
+  		h: radarheight,
+	  	maxValue: 16,
+	  	minValue: 13,
+	  	levels: 6,
+	  	ExtraWidthX: 300
+	  	//color: radarColorScale
+	}*/
+	change(maxScoreData)
+	//RadarChart.draw("#allaroundgraph", maxScoreData, mycfg);
+	function selectDataset() {
+		var value = +this.value;
+		console.log(value);
+		if (value == 0) {
+			change(maxScoreData);
+		} else {
+			change(avgScoreData);
+		}
+		
+	}
+	function change(dataset) {
+		mycfg = generatecfg(dataset);
+		RadarChart.draw("#allaroundgraph", dataset, mycfg);
+	}
+	function generatecfg(dataset) {
+		var maxValueVT = d3.max(dataset, function(d) {  return d.axes[0].value})
+	  	var minValueVT = d3.min(dataset, function(d) {  return d.axes[0].value})
+	  	var maxValueUB = d3.max(dataset, function(d) {  return d.axes[1].value})
+	  	var minValueUB = d3.min(dataset, function(d) {  return d.axes[1].value})
+	  	var maxValueBB = d3.max(dataset, function(d) {  return d.axes[2].value})
+	  	var minValueBB = d3.min(dataset, function(d) {  return d.axes[2].value})
+	  	var maxValueFX = d3.max(dataset, function(d) {  return d.axes[3].value})
+	  	var minValueFX = d3.min(dataset, function(d) {  return d.axes[3].value})
+	  	/*console.log(maxValueVT);
+	  	console.log(minValueVT);
+	  	console.log(maxValueUB);
+	  	console.log(minValueUB);
+	  	console.log(maxValueBB);
+	  	console.log(minValueBB);
+	  	console.log(maxValueFX);
+	  	console.log(minValueFX);*/
+	  	maxValues = [maxValueVT, maxValueUB, maxValueBB, maxValueFX]
+	  	minValues = [minValueVT, minValueUB, minValueBB, minValueFX]
+	  	var mycfg = {
+	  		w: radarwidth,
+	  		h: radarheight,
+		  	maxValues: maxValues,
+		  	minValues: minValues,
+		  	levels: 6,
+		  	ExtraWidthX: 300
+		  	//color: radarColorScale
+		}
+		return mycfg;
+	}
 });
 
 
@@ -65,3 +144,4 @@ function cast(d) {
 function round3(num) {
 	return parseFloat(Math.round(num * 1000) / 1000).toFixed(3);
 }
+
